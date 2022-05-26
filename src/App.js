c09bebda0601a6686500ccf8916c11ab777f6b75
Dmitriy <dima.ivahnoff@mail.ru>
2022-05-26 18:19:50 +0300

    backend mockApi

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import './scss/app.scss'
+import React from 'react'
 import Header from './components/Header'
 import Categories from './components/Categories'
 import Sort from './components/Sort'
 import PizzaBlock from './components/PizzaBlock'
-import pizzas from './assets/pizzas.json'
 
 function App() {
-  return (
-    <div className="wrapper">
-        <Header />
-        <div className="content">
-            <div className="container">
-                <div className="content__top">
-                    <Categories />
-                    <Sort />
-                </div>
-                <h2 className="content__title">Все пиццы</h2>
-                <div className="content__items">
-                    {
-                        pizzas.map((obj) => (
-                            <PizzaBlock key={obj.id} {...obj}/>
-                        ))
-                    }
-                    
+
+    const [items, setItems] = React.useState([])
+
+    React.useEffect(() => {
+        fetch('https://628f92a1dc478523654307e6.mockapi.io/items').then((res) => res.json()) 
+        .then(arr => {
+            setItems(arr)
+        })
+    }, [])
+
+    
+
+    return (
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories />
+                        <Sort />
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        {
+                            items.map((obj) => (
+                                <PizzaBlock key={obj.id} {...obj}/>
+                            ))
+                        }
+                        
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
